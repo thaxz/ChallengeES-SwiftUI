1d44cd2c7d0185e1f74767d902d7640e286b7479
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-05 14:27:42 -0300

    first commit

---
+++ ToDoSwiftUI/ToDoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftUIApp.swift
+//  ToDoSwiftUI
+//
+//  Created by thaxz on 05/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
