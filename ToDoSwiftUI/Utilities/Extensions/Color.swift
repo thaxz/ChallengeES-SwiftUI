a6b0947b0f27681c99e45937ed1eeb9ab4eb2158
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-05 14:55:54 -0300

    feature(Colors): Adding color scheme

---
+++ ToDoSwiftUI/Utilities/Extensions/Color.swift
@@ -0,0 +1,24 @@
+//
+//  Color.swift
+//  ToDoSwiftUI
+//
+//  Created by thaxz on 05/10/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let background = Color("BgColor")
+    let customGray = Color("CustomGrayColor")
+    let date = Color("DateColor")
+    let simpleText = Color("SimpleTextColor")
+    let title = Color("TitleColor")
+}
